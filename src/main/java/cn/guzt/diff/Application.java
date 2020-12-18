7ce874d511e1c73e41d007e38d1c76ec10a6a9d2
MYWAY\guzt <guzhongtaoocp@126.com>
2020-12-18 18:28:05 +0800

    BUG处理

--- src/main/java/cn/guzt/diff/Application.java
+++ src/main/java/cn/guzt/diff/Application.java
@@ -22,25 +22,25 @@ public class Application {
         // 数据库的唯一标识
         originalDb.setInstanceId("DEV-srm");
         // 数据库名称
-        originalDb.setDbName("tiannake-srm-api-dev");
+        originalDb.setDbName("bbb");
         // 数据库路径
-        originalDb.setHost("rm-bp184x857665yd5yfio.mysql.rds.aliyuncs.com");
+        originalDb.setHost("xxxx");
         // 端口号
         originalDb.setPort(3306);
         // 登录名
         originalDb.setUser("root");
         // 登录密码
-        originalDb.setPassword("middol8801_");
+        originalDb.setPassword("root123");
 
         // 远程的数据库配置信息 一般指运维库
         DbProperties targetDb = new DbProperties();
         // 数据库的唯一标识
         targetDb.setInstanceId("PROD-srm");
-        targetDb.setDbName("tiannake_srm_prod");
-        targetDb.setHost("rm-uf6rfdb0w5t837c0q9o.mysql.rds.aliyuncs.com");
+        targetDb.setDbName("bbb");
+        targetDb.setHost("ipxxxxx");
         targetDb.setPort(3306);
-        targetDb.setUser("tnk_readonly");
-        targetDb.setPassword("tnkSRM2020_RONY");
+        targetDb.setUser("root");
+        targetDb.setPassword("root123");
 
         // 执行比对的action方法
         // 导出的比对结果文件在   工程文件根路径/mysqldiff/export/  路径下
